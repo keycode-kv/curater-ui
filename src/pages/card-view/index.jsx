db7141c3215c3193f238cecdbbcefa50ae020459
Vinayak <vinayak@keyvalue.systems>
2023-09-16 05:48:54 +0530

    fix style

--- src/pages/card-view/index.jsx
+++ src/pages/card-view/index.jsx
@@ -369,7 +369,7 @@ const useMobileCardViewStyles = makeStyles({
   cardBody: {
     overflowY: "auto",
     overflowX: "auto",
-    marginTop: -100,
+    // marginTop: -100,
   },
   rating: {
     marginTop: 16,
